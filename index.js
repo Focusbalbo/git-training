061851a7667f48a7326cd6b17f9ca6c275a2ec61
Focusbalbo <stonne610@hotmail.com>
2018-12-06 10:58:56 +0100

    Update index.js
    C'est ici le changement final

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("Voila encore un autre changement");
-=======
-console.log("Hé j'ai changé");
->>>>>>> daea15a24088dd26f48c9dbd7b1a051fada86090
+console.log("le changement final");
